cf23a15e4be23328f4d7c11e70986894216c5d78
lou <lou@Mac.localdomain>
2024-12-08 23:40:37 +0800

    remove errors causing build to fail

--- src/app/pair/[address]/request/page.tsx
+++ src/app/pair/[address]/request/page.tsx
@@ -1,14 +1,7 @@
-interface PageProps {
-    params: {
-      address: string
-    }
-  }
-  
-  export default function RequestPage({ params }: PageProps) {
-    return (
-      <main className="container">
-        <h1>Request Sponsored Post</h1>
-        <p>Market Address: {params.address}</p>
-      </main>
-    )
-  }
+export default function RequestPage() {
+  return (
+    <main className="container">
+      <h1>Request Sponsored Post</h1>
+    </main>
+  );
+}
