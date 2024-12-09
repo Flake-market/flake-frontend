7d7572064c33bfb7bba1858b97da6e07dd9bacc1
lou <lou@Mac.localdomain>
2024-12-10 01:50:40 +0800

    add sanitize input function to deal with chars in inputs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sanitizeInput(value: string): string {
+  const sanitized = value.replace(/[^0-9.]/g, '');
+  const parts = sanitized.split('.');
+  if (parts.length > 2) {
+    parts.pop();
+  }
+  return parts.join('.');
+}
